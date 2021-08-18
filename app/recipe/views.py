1de64b7fe66d19607be1eac56ac055a294b89529
stephen <stephen@trialing.org>
2021-08-18 18:27:12 +0200

    last commit not working, but np

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -10,7 +10,6 @@ from core.models import Tag, Recipe
 from recipe import serializers
 
 
-
 class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
     """Manage tags in the database"""
     authentication_classes = (TokenAuthentication,)
@@ -36,20 +35,25 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         """Retrieve the recipes for the authenticated user"""
-        return self.queryset.filter(user=self.request.user)
-
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only: queryset = queryset.filter(recipe__isnull=False)
+        return queryset.filter( user=self.request.user).order_by('-name').distinct()
 
     def get_serializer_class(self):
         """Return appropriate serializer class"""
-        if self.action == 'retrieve': return serializers.RecipeDetailSerializer
-        elif self.action == 'upload_image': return serializers.RecipeImageSerializer
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
 
         return self.serializer_class
 
     def perform_create(self, serializer):
         """Create a new recipe"""
         serializer.save(user=self.request.user)
-
 
     @action(methods=['POST'], detail=True, url_path='upload-image')
     def upload_image(self, request, pk=None):
