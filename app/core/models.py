7bfcacb092f1c1efbae4e5bd946f859f31a4056b
stephen <stephen@trialing.org>
2021-08-18 16:45:22 +0200

    Added recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -3,12 +3,15 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.conf import settings
 from django.db.models.deletion import CASCADE
 
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new User"""
-        if not email: raise ValueError('Users must have an email address')
-        user = self.model(email= self.normalize_email( email ), **extra_fields) # ** allow us to add extra params 
+        if not email:
+            raise ValueError('Users must have an email address')
+        # ** allow us to add extra params
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
@@ -58,3 +61,20 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
