95e44b99116a6837680fd3eae8eca087d7544f43
stephen <stephen@trialing.org>
2021-08-18 14:30:37 +0200

    added list ingredient

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,36 +2,36 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from core import models
 
+
 def sample_user(email='test@trialing.org', password='testpass'):
     """"" Create a user"""
     return get_user_model().objects.create_user(email, password)
 
-class ModelTests( TestCase ):
+
+class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """"" Tets creating a new user with an email is successful"""
         email = 'test@trialing.org'
         password = 'Test123456'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
+            email=email,
+            password=password
         )
 
-        self.assertEqual( user.email, email)
-        self.assertTrue( user.check_password( password ) )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
         email = 'test@TRIALING.org'
         user = get_user_model().objects.create_user(email, 'test123')
-        
-        self.assertEqual(user.email, email.lower())
 
+        self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
-
 
     def test_new_superuser(self):
         """Test creating a new superuser"""
@@ -43,10 +43,16 @@ class ModelTests( TestCase ):
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
 
-
     def test_tag_str(self):
         """" Test the tag string representation"""
-        tag = models.Tag.objects.create( user = sample_user(), name= 'Vegan')
-        self.assertEqual( str(tag), tag.name)
+        tag = models.Tag.objects.create(user=sample_user(), name='Vegan')
+        self.assertEqual(str(tag), tag.name)
 
+    def test_ingredient_str(self):
+        """Test the ingredient string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Cucumber'
+        )
 
+        self.assertEqual(str(ingredient), ingredient.name)
---
+++ app/ingredient/serializers.py
@@ -0,0 +1,11 @@
+from core.models import Ingredient
+from rest_framework import serializers
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for an ingredient object"""
+
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
